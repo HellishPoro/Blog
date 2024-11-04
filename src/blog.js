11026b6f1809a79e5e4ec50c97414884581064d0
Leonid <borl99@inbox.ru>
2024-11-04 19:38:35 +0300

    Реализовать шапку

--- src/blog.js
+++ src/blog.js
@@ -1,5 +1,16 @@
 import { Routes, Route } from 'react-router-dom';
+import { Header } from './components';
 import styled from 'styled-components';
+
+const AppColumn = styled.div`
+	display: flex;
+	flex-direction: column;
+	justify-content: space-between;
+	width: 1000px;
+	min-height: 100%;
+	margin: 0 auto;
+	background-color: #fff;
+`;
 
 const Content = styled.div`
 	padding: 120px 0;
@@ -10,12 +21,15 @@ const H2 = styled.h2`
     text-align: center;
 `;
 
-const Header = () => <div>Шапка</div>
+
+
 const Footer = () => <div>Футер</div>
+
+
 
 function Blog() {
 	return (
-		<>
+		<AppColumn>
 			<Header />
 			<Content>
 				<H2>Контент страницы</H2>
@@ -30,7 +44,7 @@ function Blog() {
 				</Routes>
 			</Content>
 			<Footer />
-		</>
+		</AppColumn>
 	);
 }
 
